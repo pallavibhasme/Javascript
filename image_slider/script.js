64b3fffd7f67377d58478a04931a40b98202e6a2
Pallavi Bhasme <87770280+pallavibhasme@users.noreply.github.com>
2025-01-15 23:34:55 +0530

    Add files via upload

---
+++ image_slider/script.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector('#btn');
+var div = document.querySelector(".contener");
+
+btn.addEventListener('click', function(){
+    div.classList.toggle('display');
+})
+
+
+
+
